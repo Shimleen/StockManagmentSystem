6e943f3f9d977f1dadf104a41c580a3adce9cf71
Shimleen <shimleen@gmail.com>
2019-02-16 01:18:55 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Group practice for "Stock Management System"
---
+++ StockManagementSystem/StockInUi.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace StockManagementSystem
+{
+    partial class StockInUi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.companyLabel = new System.Windows.Forms.Label();
+            this.itemLabel = new System.Windows.Forms.Label();
+            this.reorderLevelLabel = new System.Windows.Forms.Label();
+            this.availableQuantityLabel = new System.Windows.Forms.Label();
+            this.stockInQuantityLabel = new System.Windows.Forms.Label();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.companyComboBox = new System.Windows.Forms.ComboBox();
+            this.itemComboBox = new System.Windows.Forms.ComboBox();
+            this.reorderLevelTextBox = new System.Windows.Forms.TextBox();
+            this.availableQuantityTextBox = new System.Windows.Forms.TextBox();
+            this.stockInQuantityTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // companyLabel
+            // 
+            this.companyLabel.AutoSize = true;
+            this.companyLabel.Location = new System.Drawing.Point(49, 39);
+            this.companyLabel.Name = "companyLabel";
+            this.companyLabel.Size = new System.Drawing.Size(51, 13);
+            this.companyLabel.TabIndex = 0;
+            this.companyLabel.Text = "Company";
+            // 
+            // itemLabel
+            // 
+            this.itemLabel.AutoSize = true;
+            this.itemLabel.Location = new System.Drawing.Point(49, 85);
+            this.itemLabel.Name = "itemLabel";
+            this.itemLabel.Size = new System.Drawing.Size(27, 13);
+            this.itemLabel.TabIndex = 1;
+            this.itemLabel.Text = "Item";
+            // 
+            // reorderLevelLabel
+            // 
+            this.reorderLevelLabel.AutoSize = true;
+            this.reorderLevelLabel.Location = new System.Drawing.Point(49, 136);
+            this.reorderLevelLabel.Name = "reorderLevelLabel";
+            this.reorderLevelLabel.Size = new System.Drawing.Size(74, 13);
+            this.reorderLevelLabel.TabIndex = 2;
+            this.reorderLevelLabel.Text = "Reorder Level";
+            // 
+            // availableQuantityLabel
+            // 
+            this.availableQuantityLabel.AutoSize = true;
+            this.availableQuantityLabel.Location = new System.Drawing.Point(49, 180);
+            this.availableQuantityLabel.Name = "availableQuantityLabel";
+            this.availableQuantityLabel.Size = new System.Drawing.Size(92, 13);
+            this.availableQuantityLabel.TabIndex = 3;
+            this.availableQuantityLabel.Text = "Available Quantity";
+            // 
+            // stockInQuantityLabel
+            // 
+            this.stockInQuantityLabel.AutoSize = true;
+            this.stockInQuantityLabel.Location = new System.Drawing.Point(49, 226);
+            this.stockInQuantityLabel.Name = "stockInQuantityLabel";
+            this.stockInQuantityLabel.Size = new System.Drawing.Size(89, 13);
+            this.stockInQuantityLabel.TabIndex = 4;
+            this.stockInQuantityLabel.Text = "Stock In Quantity";
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(292, 244);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 5;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            // 
+            // companyComboBox
+            // 
+            this.companyComboBox.FormattingEnabled = true;
+            this.companyComboBox.Location = new System.Drawing.Point(163, 36);
+            this.companyComboBox.Name = "companyComboBox";
+            this.companyComboBox.Size = new System.Drawing.Size(121, 21);
+            this.companyComboBox.TabIndex = 6;
+            // 
+            // itemComboBox
+            // 
+            this.itemComboBox.FormattingEnabled = true;
+            this.itemComboBox.Location = new System.Drawing.Point(163, 77);
+            this.itemComboBox.Name = "itemComboBox";
+            this.itemComboBox.Size = new System.Drawing.Size(121, 21);
+            this.itemComboBox.TabIndex = 7;
+            // 
+            // reorderLevelTextBox
+            // 
+            this.reorderLevelTextBox.Location = new System.Drawing.Point(163, 129);
+            this.reorderLevelTextBox.Name = "reorderLevelTextBox";
+            this.reorderLevelTextBox.Size = new System.Drawing.Size(121, 20);
+            this.reorderLevelTextBox.TabIndex = 8;
+            // 
+            // availableQuantityTextBox
+            // 
+            this.availableQuantityTextBox.Location = new System.Drawing.Point(163, 180);
+            this.availableQuantityTextBox.Name = "availableQuantityTextBox";
+            this.availableQuantityTextBox.Size = new System.Drawing.Size(121, 20);
+            this.availableQuantityTextBox.TabIndex = 9;
+            // 
+            // stockInQuantityTextBox
+            // 
+            this.stockInQuantityTextBox.Location = new System.Drawing.Point(163, 219);
+            this.stockInQuantityTextBox.Name = "stockInQuantityTextBox";
+            this.stockInQuantityTextBox.Size = new System.Drawing.Size(121, 20);
+            this.stockInQuantityTextBox.TabIndex = 10;
+            // 
+            // StockInUi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 299);
+            this.Controls.Add(this.stockInQuantityTextBox);
+            this.Controls.Add(this.availableQuantityTextBox);
+            this.Controls.Add(this.reorderLevelTextBox);
+            this.Controls.Add(this.itemComboBox);
+            this.Controls.Add(this.companyComboBox);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.stockInQuantityLabel);
+            this.Controls.Add(this.availableQuantityLabel);
+            this.Controls.Add(this.reorderLevelLabel);
+            this.Controls.Add(this.itemLabel);
+            this.Controls.Add(this.companyLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "StockInUi";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Stock In";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label companyLabel;
+        private System.Windows.Forms.Label itemLabel;
+        private System.Windows.Forms.Label reorderLevelLabel;
+        private System.Windows.Forms.Label availableQuantityLabel;
+        private System.Windows.Forms.Label stockInQuantityLabel;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.ComboBox companyComboBox;
+        private System.Windows.Forms.ComboBox itemComboBox;
+        private System.Windows.Forms.TextBox reorderLevelTextBox;
+        private System.Windows.Forms.TextBox availableQuantityTextBox;
+        private System.Windows.Forms.TextBox stockInQuantityTextBox;
+    }
+}
